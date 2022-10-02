1cc10fd3283d260d302c9c53b6be9409ca126f9b
elsa33eed <elsa33eed@gmail.com>
2022-10-02 21:37:37 +0200

    Update

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// change background automaticlly
+const background = document.querySelector(".landing"); //select element to change background
+const imageArr = ["01.png", "02.png", "03.png", "04.png"]; // make array with background images
+setInterval(() => {
+  const randomNum = Math.floor(Math.random() * imageArr.length); // make random number
+  background.style.backgroundImage = 'url("image/' + imageArr[randomNum] + '")'; //change background
+}, 10000);
