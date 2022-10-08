08df3778b195b018d003ede70f50d95670e4bc6a
elsa33eed <elsa33eed@gmail.com>
2022-10-08 15:42:15 +0200

    Add scroll to top button

--- js/main.js
+++ js/main.js
@@ -23,3 +23,22 @@ window.onscroll = function () {
     });
   }
 };
+
+// start scroll to top buttom
+let span = document.querySelector(".up");
+
+window.onscroll = function () {
+  // console.log(this.scrollY);
+  if (this.scrollY >= 600) {
+    span.classList.add("show");
+  } else {
+    span.classList.remove("show");
+  }
+};
+
+span.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
