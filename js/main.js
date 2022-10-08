cd6faad1d54e30c9af66bc924b1e4678a38db42d
elsa33eed <elsa33eed@gmail.com>
2022-10-08 13:34:49 +0200

    Add Skills Section

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 // change background automaticlly
 const background = document.querySelector(".landing"); //select element to change background
-const imageArr = ["01.png", "02.png", "03.png", "04.png"]; // make array with background images
+const imageArr = ["01.png", "02.png", "03.png", "04.png", "05.png", "06.png"]; // make array with background images
 setInterval(() => {
   const randomNum = Math.floor(Math.random() * imageArr.length); // make random number
   background.style.backgroundImage = 'url("image/' + imageArr[randomNum] + '")'; //change background
 }, 20000);
+
+// animate data progress
+let ourSkills = document.querySelector(".skills");
+window.onscroll = function () {
+  let positionTop = ourSkills.offsetTop;
+  let skillsHeight = ourSkills.offsetHeight;
+  let windowsHeight = this.innerHeight;
+  let myScroll = this.pageYOffset;
+
+  if (myScroll > positionTop + skillsHeight - windowsHeight) {
+    let dataProgress = document.querySelectorAll(
+      ".skills .container .skill-box .skill-progress span"
+    );
+    dataProgress.forEach((span) => {
+      span.style.width = span.dataset.progress;
+    });
+  }
+};
